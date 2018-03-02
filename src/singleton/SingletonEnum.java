d6de97546cf770339f99c911e7d9627cdc0446a2
samokat <samokatanet@gmail.ru>
2018-03-02 20:39:31 +0300

    no message

--- src/singleton/SingletonEnum.java
+++ src/singleton/SingletonEnum.java
@@ -1,5 +1,17 @@
 package singleton;
 
 public enum SingletonEnum {
-    SINGLETON;
+    SINGLETON(1) {
+        @Override
+        void printSingleton() {
+            System.out.println("Im here!" + number);
+        }
+    };
+    int number;
+    SingletonEnum(int p) {
+        number = p;
+    }
+
+    abstract void printSingleton();
+
 }
